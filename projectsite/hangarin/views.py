b5a19e624148a29fb824b606370b8850079024a9
Jays <111653199+JophilGulane@users.noreply.github.com>
2025-09-20 17:19:39 +0800

    added static bootstrap

--- projectsite/hangarin/views.py
+++ projectsite/hangarin/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from hangarin.models import Category, Task, SubTask, Note, Priority
+
+class HomePageView(ListView):
+    model = Category
+    context_object_name = 'home'
+    template_name = "home.html"
