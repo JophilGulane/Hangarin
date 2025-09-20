6a5cc5c74b78849c9b3734dc027469716ec108cf
Jays <111653199+JophilGulane@users.noreply.github.com>
2025-09-20 20:12:03 +0800

    change bootstrap template

--- projectsite/hangarin/views.py
+++ projectsite/hangarin/views.py
@@ -8,3 +8,9 @@ class HomePageView(ListView):
     model = Category
     context_object_name = 'home'
     template_name = "home.html"
+    
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'task_list.html'
+    paginate_by = 5
